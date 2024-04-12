d73098bbcc8436ad11169eb00ad8705f5073628e
Voming <ebomin1234@gmail.com>
2024-04-12 17:42:04 +0900

    SGROUP 기본세팅완료

---
+++ swithme/src/main/java/swithme/model/group/controller/GroupController.java
@@ -0,0 +1,51 @@
+package swithme.model.group.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import swithme.model.group.dto.GroupDto;
+import swithme.model.group.service.GroupService;
+
+/**
+ * Servlet implementation class GroupController
+ */
+@WebServlet("/group")
+public class GroupController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	GroupService service = new GroupService();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GroupController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<GroupDto> grouplist = service.selectAllList();
+		System.out.println(grouplist);
+		request.getSession().setAttribute("grouplist", grouplist);
+
+		request.getRequestDispatcher("/WEB-INF/views/grouphome.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

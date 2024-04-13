2fdc0dcc1da3ad550313516a051892abd529d840
singas0ng <juliachaewon@naver.com>
2024-04-13 14:42:47 +0900

    BoardListDto 생성(게시글 목록)

---
+++ swithme/src/main/java/swithme/model/board/dto/BoardListDto.java
@@ -0,0 +1,48 @@
+package swithme.model.board.dto;
+
+public class BoardListDto {
+
+	private int boardId;
+	private String title;
+	private String boardWriter;
+	private String writeTime;
+	private int readCount;
+	
+	@Override
+	public String toString() {
+		return "BoardListDto [boardId=" + boardId + ", title=" + title + ", boardWriter=" + boardWriter + ", writeTime="
+				+ writeTime + ", readCount=" + readCount + "]";
+	}
+
+	public BoardListDto(int boardId, String title, String boardWriter, String writeTime, int readCount) {
+		super();
+		this.boardId = boardId;
+		this.title = title;
+		this.boardWriter = boardWriter;
+		this.writeTime = writeTime;
+		this.readCount = readCount;
+	}
+
+	public int getBoardId() {
+		return boardId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getBoardWriter() {
+		return boardWriter;
+	}
+
+	public String getWriteTime() {
+		return writeTime;
+	}
+
+	public int getReadCount() {
+		return readCount;
+	}
+	
+	
+	
+}

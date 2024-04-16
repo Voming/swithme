8589d2783f2e90d25bcf09a83ccd2d57178416fe
oyeee <no.931006@gmail.com>
2024-04-16 16:43:08 +0900

    MemberUpdateDto 생성

---
+++ swithme/src/main/java/swithme/model/member/dto/MemberUpdateDto.java
@@ -0,0 +1,40 @@
+package swithme.model.member.dto;
+
+public class MemberUpdateDto {
+
+	private String memId;
+	private String memPwd;
+	private String memPwdNew;
+	private String memEmail;
+	
+	@Override
+	public String toString() {
+		return "MemberUpdateDto [memId=" + memId + ", memPwd=" + memPwd + ", memPwdNew=" + memPwdNew + ","+ 
+	" memEmail="+ memEmail + "]";
+	}
+	
+	public MemberUpdateDto(String memId, String memPwd, String memPwdNew, String memEmail) {
+		super();
+		this.memId = memId;
+		this.memPwd = memPwd;
+		this.memPwdNew = memPwdNew;
+		this.memEmail = memEmail;
+	}
+	
+	public String getMemId() {
+		return memId;
+	}
+	public String getMemPwd() {
+		return memPwd;
+	}
+	public String getMemPwdNew() {
+		return memPwdNew;
+	}
+	public String getMemEmail() {
+		return memEmail;
+	}
+	
+
+	
+	
+}

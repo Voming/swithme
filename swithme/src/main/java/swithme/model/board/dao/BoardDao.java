58dc246301bef2d4b5e6f6248ceea8a5e56b78c8
singas0ng <juliachaewon@naver.com>
2024-04-16 17:40:57 +0900

    BoardListDto 수정

--- swithme/src/main/java/swithme/model/board/dao/BoardDao.java
+++ swithme/src/main/java/swithme/model/board/dao/BoardDao.java
@@ -10,11 +10,48 @@ import java.util.List;
 import static swithme.jdbc.common.JdbcTemplate.close;
 
 import swithme.model.board.dto.BoardDto;
-import swithme.model.board.dto.BoardInsertDto;
 import swithme.model.board.dto.BoardListDto;
 
 public class BoardDao {
 
+	// select all
+		public List<BoardListDto> selectAllList(Connection conn) {
+			List<BoardListDto> result = null;
+			String sql = "SELECT BOARD_ID, TITLE, BOARD_WRITER, CONTENT, WRITE_TIME, READ_COUNT, BOARD_LIKE FROM BOARD";
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+
+//			private int boardId;
+//			private String boardWriter;
+//			private String title;
+//			private String content;
+//			private String writeTime; //timeStamp
+//			private int readCount;
+//			private int boardLike;
+
+			try {
+
+				pstmt = conn.prepareStatement(sql);
+				rs = pstmt.executeQuery();
+
+				if (rs.next()) {
+					result = new ArrayList<BoardListDto>();
+					do {
+
+						BoardListDto dto = new BoardListDto(rs.getInt("BOARD_ID"), rs.getString("TITLE"),
+								rs.getString("BOARD_WRITER"), rs.getString("WRITE_TIME"), rs.getInt("READ_COUNT"));
+						result.add(dto);
+					} while (rs.next());
+
+				}
+			} catch (SQLException e) {
+
+			}
+			close(rs);
+			close(pstmt);
+			return result;
+
+		}
 	
 	
 	//total count
@@ -39,45 +76,7 @@ public class BoardDao {
 		return result;
 	} 
 	
-	
-	// select all
-	public List<BoardListDto> selectAllList(Connection conn) {
-		List<BoardListDto> result = null;
-		String sql = "SELECT BOARD_ID, TITLE, BOARD_WRITER, CONTENT, WRITE_TIME, READ_COUNT, BOARD_LIKE FROM BOARD";
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 
-//		private int boardId;
-//		private String boardWriter;
-//		private String title;
-//		private String content;
-//		private String writeTime; //timeStamp
-//		private int readCount;
-//		private int boardLike;
-
-		try {
-
-			pstmt = conn.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-
-			if (rs.next()) {
-				result = new ArrayList<BoardListDto>();
-				do {
-
-					BoardListDto dto = new BoardListDto(rs.getInt("BOARD_ID"), rs.getString("TITLE"),
-							rs.getString("BOARD_WRITER"), rs.getString("WRITE_TIME"), rs.getInt("READ_COUNT"));
-					result.add(dto);
-				} while (rs.next());
-
-			}
-		} catch (SQLException e) {
-
-		}
-		close(rs);
-		close(pstmt);
-		return result;
-
-	}
 
 	public BoardDto selectOne(Connection conn, Integer boardId) {
 		BoardDto result = null;
@@ -104,7 +103,7 @@ public class BoardDao {
 		return result;
 	}
 
-	public int insert(Connection conn, BoardInsertDto dto) {
+	public int insert(Connection conn, BoardDto dto) {
 		int result = 0;
 		String sql = "INSERT INTO BOARD(BOARD_ID, BOARD_WRITER, TITLE, CONTENT, WRITE_TIME, READ_COUNT, BOARD_LIKE"
 				+ "VALUES(SEQ_BOARD_ID.nextval,?, ?, ?, default, default, default)";

67f53bc3134597f07c99b4f278be7408d982bcf6
oyeee <no.931006@gmail.com>
2024-04-17 16:59:05 +0900

    login controller

--- swithme/src/main/java/swithme/model/member/controller/LoginController.java
+++ swithme/src/main/java/swithme/model/member/controller/LoginController.java
@@ -7,7 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import swithme.model.member.dto.MemberInfoDto;
 import swithme.model.member.dto.MemberLoginDto;
+import swithme.model.member.service.MemberService;
 
 
 /**
@@ -37,11 +39,21 @@ public class LoginController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		String id=request.getParameter("id");
 		String pwd=request.getParameter("pwd");
 		MemberLoginDto dto=new MemberLoginDto(id,pwd);
 		
-		
+		//ajax
+				int result=0;
+				MemberInfoDto resultInfo= new MemberService().loginGetInfo(dto);		
+				if(resultInfo != null) {
+					//성공
+					request.getSession().setAttribute("loginInfo", resultInfo );
+					result=1;
+				}
+				response.getWriter().append(String.valueOf(result));
+			}
 	}
 
-}
+

f201c0342111bde92deea42dabdac1597f36fe8b
oyeee <no.931006@gmail.com>
2024-04-17 14:31:59 +0900

    join controller 생성

---
+++ swithme/src/main/java/swithme/model/member/controller/JoinController.java
@@ -0,0 +1,58 @@
+package swithme.model.member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import swithme.model.member.dto.MemberDto;
+import swithme.model.member.service.MemberService;
+
+
+
+/**
+ * Servlet implementation class JoinController
+ */
+@WebServlet("/join")
+public class JoinController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public JoinController() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/member/join.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String memId = request.getParameter("id");
+		String memPwd = request.getParameter("pwd");
+		String memEmail = request.getParameter("email");
+		
+		MemberDto dto = new MemberDto(memId, memPwd, memEmail);
+		System.out.println(dto);
+		int result = new MemberService().insert(dto);
+		if(result < 0 ) {
+			// 회원가입실패시
+			response.sendRedirect(request.getContextPath()+"/"); //TODO
+			return;
+		}
+		// 회원가입정상
+		response.sendRedirect(request.getContextPath()+"/login");
+	}
+	
+	}
+
+

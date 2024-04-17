d6e63741cc5a13bf4d6d7e4485032b51a75fe1db
singas0ng <juliachaewon@naver.com>
2024-04-17 09:57:51 +0900

    커뮤니티 controller

--- swithme/src/main/java/swithme/model/board/controller/BoardListController.java
+++ swithme/src/main/java/swithme/model/board/controller/BoardListController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class BoardListController
  */
-@WebServlet("/BoardListController")
+@WebServlet("/board")
 public class BoardListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,8 +26,7 @@ public class BoardListController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/WEB-INF/views/board/board.jsp").forward(request, response);
 	}
 
 	/**

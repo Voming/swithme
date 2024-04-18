6b640ba6922237f0792ecb5ddb4b98b82c358fac
oyeee <no.931006@gmail.com>
2024-04-18 16:09:54 +0900

    welcome controller 생성

---
+++ swithme/src/main/java/swithme/model/welcome/controller/WelcomeController.java
@@ -0,0 +1,32 @@
+package swithme.model.welcome.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeController
+ */
+@WebServlet("/welcome")
+public class WelcomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeController() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/welcome/welcome.jsp").forward(request, response);
+	}
+
+
+}

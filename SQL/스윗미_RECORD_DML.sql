2f228c859e339998db9a9f996b0d28410e73cf7e
JJOGGAN <withmelona1@gmail.com>
2024-04-19 11:33:48 +0900

    과목리스트 SQL문 수정

--- SQL/스윗미_RECORD_DML.sql
+++ SQL/스윗미_RECORD_DML.sql
@@ -6,6 +6,14 @@ exec PRO_RECORD_INSERT_DINNER('won','D_3','02');
 exec PRO_RECORD_INSERT_MORNING('won','D_1','18');
 exec PRO_RECORD_INSERT_LUNCH('won','D_2','18');
 exec PRO_RECORD_INSERT_DINNER('won','D_3','18');
+
+--19일
+exec PRO_RECORD_INSERT_MORNING('won','D_3','19');
+exec PRO_RECORD_INSERT_LUNCH('won','SQL','19');
+exec PRO_RECORD_INSERT_DINNER('won','D_1','19');
 --delete from record;
-select * from record order by record_id asc;
+select * from record where record_mem_id='won' order by record_end desc
+;
+delete from subject where subject_id>43;
+
 commit;

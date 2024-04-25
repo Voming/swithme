7ab0bc06feadb29cb9861791e77b93c86ef8a7d1
singas0ng <juliachaewon@naver.com>
2024-04-25 15:25:53 +0900

    댓글, 대댓글 list 뽑아주는 controller추가
    댓글, 대댓글 등록하는 controller와 분리
    - 오류가 어디서 왔는지 잘 구별 가능
    (댓글, 대댓글 등록시 오류가 났는지, 이미 등록된 값들 불러올때 오류 났는지 구별이 용이)
    - 그 외 그냥 나누는게 나은것같아서 분리시킴

---
+++ swithme/src/main/java/swithme/model/board/controller/BoardReplyContentController.java
@@ -0,0 +1,53 @@
+package swithme.model.board.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import swithme.model.board.dto.BoardReplyDto;
+import swithme.model.board.service.BoardService;
+
+/**
+ * Servlet implementation class BoardReplyContentController
+ */
+@WebServlet("/board/reply/content")
+public class BoardReplyContentController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	BoardService service = new BoardService();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardReplyContentController() {
+        super();
+       
+    }
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("loadedHandler 안에 있는 ajax에서 댓글, 대댓글 정보 받아오기");
+		String boardIdStr = request.getParameter("id");
+		
+		try {
+			int boardId = Integer.parseInt(boardIdStr);
+			List<BoardReplyDto> replydtolist = service.selectBoardReplyList(boardId); //댓글들 읽기
+			System.out.println(replydtolist);
+			response.getWriter().append(new Gson().toJson(replydtolist));
+		
+		} catch (NumberFormatException e) {
+			System.out.println("!!! NumberFormatException !!!!!!");
+			response.getWriter().append(new Gson().toJson(null));
+		}
+		
+	}
+
+}

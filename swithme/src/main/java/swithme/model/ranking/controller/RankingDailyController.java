d64d68cb37a2ccc37c6bd4ef6cc71275a8431e76
singas0ng <juliachaewon@naver.com>
2024-05-03 17:34:25 +0900

    일간 랭킹 controller
    - controller에서 1개 이상의 데이터를 보내야해서 db에 갔다와서 다시 결과값을 받을 떄 map 형태로 받기
    ※ map과 list 차이 간략하게 써놨으니 확인할것

--- swithme/src/main/java/swithme/model/ranking/controller/RankingDailyController.java
+++ swithme/src/main/java/swithme/model/ranking/controller/RankingDailyController.java
@@ -1,7 +1,9 @@
 package swithme.model.ranking.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -39,16 +41,32 @@ public class RankingDailyController extends HttpServlet {
 		String memId = request.getParameter("memId");
 		MemberInfoDto loginInfo = (MemberInfoDto) request.getSession().getAttribute("loginInfo");
 		
+		//이 두개의 값을 controller 통해 보냄
+		RankingDto dailyTop = service.rankingDailyTop();
+		Integer dailyTopTime = dailyTop.getDifftime();
+		//dailyTop 의 자료형이 RankingDto 라서 RankingDto 안에 있는것들 사용 가능
+		//jsp에서 css로 시간 설정할때는 1등 시간만 있으면 되서 1등 시간만 가져옴(1등 시간이 기준이 되므로)
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		//처음에는 List형태로 받았는데 값 여러개 꺼내야해서 Map으로 받음
+		//List<RankingDto> 도 넣고 dailyTopTime 도 넣기 위해 Map 사용
+		// 자료형이 섞여도 상관없음
+		map.put("dailyTopTime", dailyTopTime);
+		//보낸 값을 다시 받아서 map 형태에 넣음
+		
 		if (loginInfo != null) { // 로그인 했을 때
 			memId = loginInfo.getMemId();
 			System.out.println("em\\드감?????");
 			List<RankingDto> dto = service.rankingDaily();
+			//List에는 자료형이 하나만 들어갈 수 있음 => 여기서는 RankingDto 하나만 들어갈 수 있음
+			// 자동적으로 index 생성
+			//List는 add 했을 때 <> 안에 있는 얘들만 넣는거 가능(dto에 있는 field명과 관련된 얘들이랄까..)
 			System.out.println("일간 랭킹" + dto);
-
+			map.put("dailyTime", dto);
 			request.setAttribute("dailyTime", dto);
-			response.getWriter().append(new Gson().toJson(dto));
 
 		}
+		response.getWriter().append(new Gson().toJson(map));
 
 	}
 }
